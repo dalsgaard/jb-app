f8013a2b135fe27170e0c0e168f92313ff534e34
Kim <kim@kimdalsgaard.com>
2018-06-27 07:50:07 +0200

    Misc

--- serve.config.js
+++ serve.config.js
@@ -1,6 +1,19 @@
 const path = require('path');
+proxy = require('koa-proxy');
 
 module.exports = {
   config: './webpack/dev.config.js',
-  content: './public'
+  content: './public',
+  add: (app, middleware, options) => {
+    middleware.webpack();
+    middleware.content();
+    app.use(proxy({
+      host:  'https://en.wikipedia.org',
+      match: /^\/wikipedia\//,
+      map: path => {
+        console.log(path)
+        return `/wiki${path.substring('/wikipedia'.length)}`;
+      }
+    }));
+  }
 }
--- src/app.js
+++ src/app.js
@@ -7,11 +7,16 @@ export class App extends PolymerElement {
   static get template() {
     return html`
       <section>
-        <h3>Demo</h3>
+        <h3 id="foo" on-click="foo">Demo!!!</h3>
         <jb-foo></jb-foo>
         <jb-bar></jb-bar>
       </section>
     `;
+  }
+
+  async foo () {
+    const response = await fetch('/wikipedia/Camel_case');
+    console.log(await response.text());
   }
 
 }
